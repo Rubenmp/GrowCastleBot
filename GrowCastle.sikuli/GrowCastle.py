1af73052b05119d57b1af2f48d856321206b8455
Ruben <srmorales@correo.ugr.es>
2022-02-17 17:21:51 +0100

    Create method watch_add_if_exists

--- GrowCastle.sikuli/GrowCastle.py
+++ GrowCastle.sikuli/GrowCastle.py
@@ -1,8 +1,5 @@
 import time
 
-# Configuration
-
-
 # Auxiliary methods
 seguir = True
 def stop(evento):
@@ -15,6 +12,16 @@ def log(message):
     print("[INFO] " + message)
 
 
+def log_error(message):
+    print("[ERROR] " + message)
+    exit(1)
+
+def click_if_exists(image):
+    if exists(image):
+        click(image)
+        return True
+    return False
+
 def is_ready_to_battle():
     return exists("battle_button.png")
 
@@ -26,22 +33,31 @@ def is_inside_wave():
 def replay():
     wait("replay_button.png")
     click("replay_button.png")
-    if exists("replay_type_latest_button.png"):
-        click("replay_type_latest_button.png")
+    click_if_exists("replay_type_latest_button.png")
 
     log("Replaying latest wave")
     wait("wave_icon.png")
-    if exists("speed_1x.png"):
-        click("speed_1x.png")
+    click_if_exists("speed_1x.png")
 
     return True
 
+
+def watch_add_if_exists():
+    if not click_if_exists("show_add_button.png"):
+        return
+    wait("add_seconds_remaining.png")
+    waitVanish("add_seconds_remaining.png")
+
+    wait("add_close_button.png")
+    if not click_if_exists("add_close_button.png"):
+       log_error("It was not possible to close add button")
 
 # Program start
 log("Program started, press 'Ctrl + F1' to stop it")
 time.sleep(1) # TODO: change it to allow user to read the message
 
-replay()
+watch_add_if_exists()
+print("success")
 
 
 #while (seguir):
