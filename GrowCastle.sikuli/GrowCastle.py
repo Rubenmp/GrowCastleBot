aec5804baef5f9a2d4976aae570e66c01592da25
Ruben <srmorales@correo.ugr.es>
2022-02-17 17:06:09 +0100

    Replay button clicked

--- GrowCastle.sikuli/GrowCastle.py
+++ GrowCastle.sikuli/GrowCastle.py
@@ -1,3 +1,5 @@
+import time
+
 # Configuration
 
 
@@ -12,14 +14,30 @@ Env.addHotkey(Key.F1, KeyModifier.CTRL, stop)
 def log(message):
     print("[INFO] " + message)
 
+
 def is_ready_to_battle():
     return exists("battle_button.png")
 
 
+def is_inside_wave():
+    return exists("wave_icon.png")
+
+
+def replay():
+    wait("replay_button.png")
+    click("replay_button.png")
+    if exists("replay_type_latest_button.png"):
+        click("replay_type_latest_button.png")
+
+    log("Replaying latest wave")
+    return True
+
+
 # Program start
 log("Program started, press 'Ctrl + F1' to stop it")
+time.sleep(1) # TODO: change it to allow user to read the message
 
-
+replay()
 
 
 #while (seguir):
