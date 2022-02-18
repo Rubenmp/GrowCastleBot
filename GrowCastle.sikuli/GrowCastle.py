2dc67d7fb13472c07e5c121a52ec0772dc40f0de
Ruben <srmorales@correo.ugr.es>
2022-02-19 00:53:05 +0100

    Click on treasures

--- GrowCastle.sikuli/GrowCastle.py
+++ GrowCastle.sikuli/GrowCastle.py
@@ -1,7 +1,12 @@
 import time
 
+# Configuration
+show_logs = True
+
 # Auxiliary methods
 continue_execution = True
+
+
 def stop(event):
     global continue_execution
     log("Program aborted")
@@ -9,9 +14,14 @@ def stop(event):
     exit(0)
 
 
-
 def log(message):
-    print("[INFO] " + message)
+    if show_logs:
+        print("[INFO] " + message)
+
+
+def log_warning(message):
+    if show_logs:
+        print("[WARNING] " + message)
 
 
 def log_error(message):
@@ -51,6 +61,7 @@ def force_wait_any_pattern(patterns):
 
 
 def force_wait_any_pattern_or_max_seconds(patterns, max_seconds):
+    log("Method force_wait_any_pattern_or_max_seconds(" + str(patterns) + ", " + str(max_seconds) + ")")
     # This method return found pattern index
     seconds_searching_pattern = 0
     while True:
@@ -71,17 +82,46 @@ def force_wait_pattern(pattern):
 
 
 def force_wait_main_page():
+    log("Method force_wait_main_page")
     force_wait_pattern(get_replay_button_pattern())
     time.sleep(1)
+
+
+def battle_next_wave():
+    log("Method battle_next_wave")
+    click(get_pattern("battle_button.png", 0.8))
+    click_if_exists("replay_type_latest_button.png")
+    return wait_wave_to_finish()
 
 
 def replay_latest_wave(iteration):
     click(get_replay_button_pattern())
     click_if_exists("replay_type_latest_button.png")
     log("Replaying latest wave (times " + str(iteration) + ")")
+    return wait_wave_to_finish()
+
+
+def wait_wave_to_finish():
+    # Returns True (Victory) or False (Defeat)
+    log("Method wait_wave_to_finish")
     wait("wave_icon.png")
     click_if_exists("speed_1x.png")
-    waitVanish("wave_icon.png")
+
+    max_wave_seconds = 600
+    while max_wave_seconds > 0 and exists(get_pattern("wave_icon.png", 0.9)):
+        max_wave_seconds -= 1
+        click_if_exists("treasure.png")
+        time.sleep(1)
+    if max_wave_seconds <= 0:
+        log_error("It was not possible to wait wave to finish")
+
+    battle_result_patterns = [get_pattern("battle_end_victory.png", 0.9)]
+    pattern_index = force_wait_any_pattern_or_max_seconds(battle_result_patterns)
+    if pattern_index is None:
+        log_warning("It was not possible to determine wave result, assuming defeat.")
+        return False
+
+    return pattern_index == 1
 
 
 def is_add_in_progress():
@@ -91,15 +131,17 @@ def is_add_in_progress():
 
 
 def force_add_close():
+    log("Method force_add_close")
     max_wait = 40
     while is_add_in_progress():
         max_wait -= 1
         if max_wait <= 0:
-            log_error("Waiting add to dissappear")
+            log_error("Waiting add to disappear")
         time.sleep(1)
     force_wait_any_pattern_or_max_seconds([get_pattern("add_finished_reward_granted.png", 0.8)], 3)
 
-    close_add_button_patterns = [get_pattern("add_close_button_white_background.png", 0.9), get_pattern("add_close_button_black_background.png", 0.9)]
+    close_add_button_patterns = [get_pattern("add_close_button_white_background.png", 0.9),
+                                 get_pattern("add_close_button_black_background.png", 0.9)]
     found_pattern_index = force_wait_any_pattern(close_add_button_patterns)
     close_button = close_add_button_patterns[found_pattern_index]
     if click_if_exists(close_button):
@@ -112,6 +154,7 @@ def force_add_close():
 
 
 def watch_add_if_exists():
+    log("Method watch_add_if_exists")
     if not click_if_exists("show_add_button.png"):
         return
 
@@ -132,5 +175,6 @@ while continue_execution:
     replay_latest_wave(times)
     force_wait_main_page()
     watch_add_if_exists()
+    print("watch_add_if_exists")
 
 log("Program finished")
